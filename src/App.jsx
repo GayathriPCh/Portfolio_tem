aa9c75517b833c9b86effac3f09d6419f4fcc36e
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-03-17 12:39:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "P. Gayathri",
+  title: "Student at KL University,Hyderabad",
+  email: "p.gayathri.ch@gmail.com",
+  gitHub: "GayathriPCh",
+  instagram: "",
+  linkedIn: "gayathri-p",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
