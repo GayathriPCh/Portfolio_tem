b07dfe15926a7a7bc7a7b233799c4be18d66083b
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-03-17 12:26:53 +0530

    Add about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/wave.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at KL University, Hyderabad. I am passionate about learning new technologies and building software that makes a difference. I am also a music and space enthusiast and love to integrate my interests into my projects.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -33,10 +33,9 @@ const description =
 const skillsList = [
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Backend development using Django",
+  "MERN stack",
+  "languages: Python, JavaScript, C, Java, HTML, CSS, SQL",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "As a second-year computer science student , I am driven by a deep passion for software development and a desire to contribute to meaningful projects that make a difference. With a solid foundation in programming languages and a keen interest in emerging technologies, I strive to approach every challenge with creativity, enthusiasm, and a collaborative spirit.";
 
 const About = () => {
   return (
