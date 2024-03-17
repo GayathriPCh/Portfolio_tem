02f238dfc742bd9fe1952fa4706e3d196beffd2d
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-03-17 12:28:08 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,80 +1,67 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/design-desk.jpeg";
+import image from "../images/frame.png";
+import image2 from "../images/some.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "BeatWave- Generating Music from Images",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Using python libraries and Django framework, I created a web application that generates music from images. The application uses image processing to extract colors from the image and then uses the colors to generate music.",
+    url: "https://github.com/GayathriPCh/Image-to-music-mapping-with-python-libraries",
   },
   {
-    title: "Web Development for Beginners",
+    title: "SkyQuest - Night sky explorer",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "An experimental project using just html,css and js, to create a website that includes a gallery, an event calendar and blogs about astronomy.",
+    url: "https://github.com/GayathriPCh/SKYQUEST",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Steller society - a space-themed chat integrated music player",
+    description: "A collaborative project and under development.",
+    url: "https://github.com/tmanas06/Stellar_society_project",
   },
 ];
 
 const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <img className="background" src={image2} alt="" />
+      <h2
+        style={{ textAlign: "center", color: "white", backgroundColor: "black", padding: "10px" }}
+      >
+        Portfolio
+      </h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center", padding: "10px" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            style={{ height: "80%", width: "80%", objectFit: "cover", padding: "10px" }}
             alt={imageAltText}
           />
         </div>
         <div className="container">
           {projectList.map((project) => (
-            <div className="box" key={project.title}>
+            <div
+              className="box"
+              key={project.title}
+              style={{
+                padding: "10px",
+                color: "black", // Text color
+                backgroundColor: "white", // Background color
+                marginBottom: "10px",
+                fontFamily: "",
+              }}
+            >
               <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+                <h3 style={{ flexBasis: "80px", fontWeight: "bold", color: "black" }}>
+                  {project.title}
+                </h3>
               </a>
-              <p className="small">{project.description}</p>
+              <p className="small" style={{ color: "black" }}>
+                {project.description}
+              </p>
             </div>
           ))}
         </div>
